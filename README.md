f340950fb5666c485f088de420e3086e239370ce
basspies <haroldspies@hotmail.com>
2026-04-14 11:18:47 +0200

    niks meer

--- README.md
+++
@@ -1,1 +0,0 @@
-# missKARIN-Promo-video-website-
--- assets/includes/footer.php
+++
@@ -1,4 +0,0 @@
-copyright &copy; 2024 <a href="https://www.itsupport247.com" target="_blank">IT Support 24/7</a>. All rights reserved.
-    </div>
-</body>
-</html>
